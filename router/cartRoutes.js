111478f0b841ea3da85aad70396755d5d2d80175
VijaaY17 <vijayrajendran03@gmail.com>
2023-08-07 12:45:23 +0530

    cart alteration

--- router/cartRoutes.js
+++ router/cartRoutes.js
@@ -1,22 +1,12 @@
 const express = require('express');
 const router = express.Router();
+
 const cartController = require('../controllers/cartController');
-// const isAdmin = require('../middleware/authorization').isAdmin;
-// const isUser = require('../middleware/authorization').isUser;
 
-// Create a new cart
-router.post('/carts', cartController.createCart);
-
-// Read all carts
-router.get('/carts', cartController.getAllCarts);
-
-// Read a specific cart by ID
-router.get('/carts/:id', cartController.getCartById);
-
-// Update a cart by ID
-router.put('/carts/:id', cartController.updateCart);
-
-// Delete a cart by ID
-router.delete('/carts/:id', cartController.deleteCart);
+router.get('/:id', cartController.getCartByUserId);
+router.delete('/:id', cartController.deleteCart);
+router.post('/:id', cartController.createCartItem);
+router.put('/:id/:item_id', cartController.updateCartItem);
+router.delete('/:id/:item_id', cartController.deleteCartItem);
 
 module.exports = router;
