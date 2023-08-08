5d506922ccf083cca1c1c25e83534ac6aa65e100
ragunandhant <raja.g2020cse@sece.ac.in>
2023-08-08 14:48:56 +0530

    git deleted

--- models/database.js
+++
@@ -1,9 +0,0 @@
-const Sequlize = require('sequelize')
-
-
-
-const sequelize = new Sequlize("billsmart", "root", "Sece@2021", {
-    dialect: "mysql",
-    logging: false
-})
-module.exports = sequelize
