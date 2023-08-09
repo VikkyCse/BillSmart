010f8bc6e48bd1a3b3bd11c7561065ccbc20e896
ragunandhant <raja.g2020cse@sece.ac.in>
2023-08-09 21:04:31 +0530

    gerating qr

--- qrGenrator.js
+++ qrGenrator.js
@@ -1,21 +1,19 @@
 const qr = require('qrcode');
 const fs = require('fs');
-
-function generateQRCode(data, filename) {
+const path = require('path');
+function generateQRCode(data) {
+    const writepath = path.join(__dirname, 'images', `${data}.png`)
     qr.toDataURL(data, (err, qrDataURL) => {
         if (err) {
             console.error(err);
             return;
         }
 
+
         // Save QR code image to a file
-        fs.writeFileSync(filename, qrDataURL.split(',')[1], 'base64');
-        console.log(`QR code generated and saved as ${filename}`);
+        fs.writeFileSync(writepath, qrDataURL.split(',')[1], 'base64');
+        console.log(`QR code generated and saved as ${writepath}`);
+        return writepath;
     });
 }
-
-// Information you want to encode in the QR code
-const qrData = "Hello, this is a QR code example.";
-
-// Call the function to generate QR code
-generateQRCode(qrData, 'qrcode.png');
+module.exports = generateQRCode
