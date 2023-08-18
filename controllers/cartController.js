bea33cb4a4e695ea62f38bed5deb26c902e57484
VijaaY17 <vijayrajendran03@gmail.com>
2023-08-18 09:06:13 +0530

    cart decrement

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -3,28 +3,26 @@ const CartItem = require('../models/Cart_Items');
 const Item = require('../models/Item');
 const sequelize = require('../models/database');
 
+// Error Messages
+const ERR_CART_NOT_FOUND = 'Cart not found';
+const ERR_ITEM_NOT_FOUND = 'Associated item not found';
+const ERR_REQUESTED_COUNT_EXCEEDS_QUANTITY = 'Requested count exceeds available quantity';
 
-
-// Read a specific cart by user ID
 const getCartByUserId = async (req, res) => {
   try {
     const user_id = req.params.id;
-
-    // Find the cart associated with the user_id
-    const cart = await Cart.findOne({ where: { user_id: user_id } });
+    const cart = await Cart.findOne({ where: { user_id } });
 
     if (!cart) {
-      // If cart doesn't exist, create a new cart and return it
       const newCart = await Cart.create({ user_id });
       res.status(200).json(newCart);
     } else {
-      // If cart exists, fetch its associated items
       const cartItems = await CartItem.findAll({
         where: { Cart_id: cart.id },
         include: [Item],
       });
       res.status(200).json({
-        cart: cart,
+        cart,
         items: cartItems,
       });
     }
@@ -37,15 +35,15 @@ const deleteCart = async (req, res) => {
   try {
     const user_id = req.params.id;
 
-    const cart = await Cart.findOne({ where: { user_id: user_id } });
+    const cart = await Cart.findOne({ where: { user_id } });
 
     if (!cart) {
-      res.status(404).json({ error: 'Cart not found' });
+      res.status(404).json({ error: ERR_CART_NOT_FOUND });
       return;
     }
 
     await CartItem.destroy({ where: { Cart_id: cart.id } });
-    await Cart.destroy({ where: { user_id: user_id } });
+    await Cart.destroy({ where: { user_id } });
 
     res.status(204).end(); // 204 No Content - Successfully deleted
   } catch (err) {
@@ -53,33 +51,29 @@ const deleteCart = async (req, res) => {
   }
 };
 
-
-// Create a cart item
 const createCartItem = async (req, res) => {
   const t = await sequelize.transaction();
   try {
     const { Item_id, Count, user_id } = req.body;
 
-    const [cart, created] = await Cart.findOrCreate({ where: { user_id: user_id } });
+    const [cart, created] = await Cart.findOrCreate({ where: { user_id } });
     const item = await Item.findByPk(Item_id);
 
     if (!item) {
       throw new Error('Invalid Item');
-      return
-    }
-    // console.log(item.Count);
-    if (item.quantity < Count) {
-      throw new Error('Invalid Count');
-      return
     }
 
-   
-    
+    if (item.quantity < Count) {
+      throw new Error('Invalid Count');
+    }
 
     const Cart_id = cart.id;
 
     const cartItem = await CartItem.create({ Item_id, Count, user_id, Cart_id });
-    await item.decrement({ 'quantity': Count });
+    await Item.update(
+      { quantity: item.quantity - Count },
+      { where: { id: item.id } }
+    );
 
     await t.commit();
     res.status(201).send(cartItem);
@@ -89,7 +83,6 @@ const createCartItem = async (req, res) => {
   }
 };
 
-//update
 const updateCartItem = async (req, res) => {
   const t = await sequelize.transaction();
   
@@ -100,23 +93,22 @@ const updateCartItem = async (req, res) => {
     const cartItem = await CartItem.findByPk(cartItemId);
     
     if (!cartItem) {
-      res.status(404).json({ error: 'Cart item not found' });
+      res.status(404).json({ error: ERR_CART_ITEM_NOT_FOUND });
       return;
     }
 
     const item = await Item.findByPk(cartItem.Item_id);
 
     if (!item) {
-      res.status(404).json({ error: 'Associated item not found' });
+      res.status(404).json({ error: ERR_ITEM_NOT_FOUND });
       return;
     }
 
     const previousCount = cartItem.Count;
     const countChange = Count - previousCount;
 
-    
-    if (item.quantity < Math.abs(countChange) && countChange>0) {
-      res.status(400).json({ error: 'Requested count exceeds available quantity' });
+    if (item.quantity < Math.abs(countChange) && countChange > 0) {
+      res.status(400).json({ error: ERR_REQUESTED_COUNT_EXCEEDS_QUANTITY });
       return;
     }
 
@@ -125,11 +117,10 @@ const updateCartItem = async (req, res) => {
       { where: { id: cartItemId } }
     );
 
-    if (countChange > 0) {
-      await item.increment({ 'quantity': countChange }, { transaction: t });
-    } else if (countChange < 0) {
-      await item.decrement({ 'quantity': Math.abs(countChange) }, { transaction: t });
-    }
+    await Item.update(
+      { quantity: item.quantity - countChange },
+      { where: { id: item.id } }
+    );
 
     await t.commit();
 
@@ -140,15 +131,12 @@ const updateCartItem = async (req, res) => {
   }
 };
 
-
-// Delete a cart item by ID
 const deleteCartItem = async (req, res) => {
   try {
-    const cartItemId = req.params.item_id; // Access item_id from the URL
+    const cartItemId = req.params.item_id;
     const deletedRows = await CartItem.destroy({ where: { id: cartItemId } });
 
     if (deletedRows === 0) {
-      // If no rows were deleted, the item might not exist or the ID is incorrect
       res.status(404).send({ error: 'Cart item not found' });
     } else {
       res.status(204).end(); // 204 No Content - Successfully deleted
@@ -157,7 +145,6 @@ const deleteCartItem = async (req, res) => {
     res.status(500).send({ error: 'Error deleting the cart item', err });
   }
 };
-
 
 module.exports = {
   getCartByUserId,
